105cc9e60f004a4f19be4906f9bc29f8877494b0
Zsofia Limbek <zsofia.limbek@gmail.com>
2024-04-08 18:48:15 +0200

    completed solution

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ def parse_input(input_string):
 
 
 def calculate_max_needed_stamps(max_price, stamp_prices):
-    # elosztom a legnagyobb értékűvel, aztán a maradékot a második legnagyobbal
-    pass
+    max_number_of_stamps = 1
+    for price in range(1, max_price + 1):
+        number_of_stamps = get_max_needed_stamps(price, stamp_prices)
+        if number_of_stamps > max_number_of_stamps:
+            max_number_of_stamps = number_of_stamps
+    return max_number_of_stamps
 
 
 def get_max_needed_stamps(price, stamp_prices):
@@ -21,3 +25,13 @@ def get_max_needed_stamps(price, stamp_prices):
         remaining_price = remaining_price % stamp
 
     return number_of_stamps
+
+
+if __name__ == '__main__':
+    input_message = """
+    Please enter the number of stamps and the max price in the first line, then the stamp prices in the second line
+    """
+    task_input = input(input_message)
+    max_price, stamp_prices = parse_input(task_input)
+    result = calculate_max_needed_stamps(max_price, stamp_prices)
+    print(result)
