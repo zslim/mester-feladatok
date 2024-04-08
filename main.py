6cdb257da4539cbcb2e8c9cd380fb3e5e58e0fff
Zsofia Limbek <zsofia.limbek@gmail.com>
2024-04-08 18:57:58 +0200

    fix: input lines & conversion

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-def parse_input(input_string):
-    lines = input_string.split("\n")
-    number_of_stamps, max_price = lines[0].split(" ")
-    stamp_prices = lines[1].split(" ")
-    assert number_of_stamps == len(stamp_prices)
-    return max_price, stamp_prices
-
-
 def calculate_max_needed_stamps(max_price, stamp_prices):
     max_number_of_stamps = 1
     for price in range(1, max_price + 1):
@@ -27,11 +19,15 @@ def get_max_needed_stamps(price, stamp_prices):
     return number_of_stamps
 
 
-if __name__ == '__main__':
-    input_message = """
-    Please enter the number of stamps and the max price in the first line, then the stamp prices in the second line
-    """
-    task_input = input(input_message)
-    max_price, stamp_prices = parse_input(task_input)
+def main():
+    input_line_1 = input("Please enter the number of stamps and the maximum price: ")
+    number_of_stamps, max_price = [int(number) for number in input_line_1.split(" ")]
+    input_line_2 = input("Please enter the stamp prices: ")
+    stamp_prices = [int(number) for number in input_line_2.split(" ")]
+    assert number_of_stamps == len(stamp_prices)
     result = calculate_max_needed_stamps(max_price, stamp_prices)
     print(result)
+
+
+if __name__ == '__main__':
+    main()
